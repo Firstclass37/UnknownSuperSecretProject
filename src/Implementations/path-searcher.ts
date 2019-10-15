e26d7815781c3111a7e86ae0ebbb2f443d41bb35
Firstclass37 <vankiselev@mail.ru>
2019-10-15 19:56:22 +0300

    рефакторинг

--- src/Implementations/path-searcher.ts
+++ src/Implementations/path-searcher.ts
@@ -5,25 +5,25 @@ import { Point } from "../Model/point";
 
 export class PathSearcher implements IPathSearcher{
 
-    SearchPath(map: IMap, start: IMapElement, end: IMapElement): IMapElement[] {
+    searchPath(map: IMap, start: IMapElement, end: IMapElement): IMapElement[] {
         throw new Error("Method not implemented.");
     }    
 
-    HasPath(map: IMap, start: IMapElement, end: IMapElement): boolean {
-        return this.SearchPath(map, start, end) != null;
+    hasPath(map: IMap, start: IMapElement, end: IMapElement): boolean {
+        return this.searchPath(map, start, end) != null;
     }
     
-    IsNeighbors(map: IMap, first: IMapElement, second: IMapElement): boolean {
+    areNeighbors(map: IMap, first: IMapElement, second: IMapElement): boolean {
         let firstPoint = map.GetIndex(first);
         let secondPoint = map.GetIndex(second);
         return Math.abs(firstPoint.X - secondPoint.X) == 1 && Math.abs(firstPoint.Y - secondPoint.Y) == 1;
     }
 
-    GetNeighbors(map: IMap, element: IMapElement): IMapElement[] {
-        return this.GetNeighborsPoints(map, map.GetIndex(element)).map(p => map.GetElement(p));
+    getNeighbors(map: IMap, element: IMapElement): IMapElement[] {
+        return this.getNeighborsPoints(map, map.GetIndex(element)).map(p => map.GetElement(p));
     }
 
-    GetNeighborsPoints(map: IMap, point: Point): Point[]
+    getNeighborsPoints(map: IMap, point: Point): Point[]
     {
         let neighbors: Point[] = [];
         for (let i = Math.abs(point.X - 1); i <= Math.min(point.X + 1, map.Map.length - 1); i++)
@@ -36,6 +36,4 @@ export class PathSearcher implements IPathSearcher{
         }
         return neighbors;
     }
-
-
 }
--- src/Interfaces/i-path-searcher.ts
+++ src/Interfaces/i-path-searcher.ts
@@ -2,11 +2,11 @@ import { IMapElement } from "./i-map-element";
 import { IMap } from "./i-map";
 
 export interface IPathSearcher{
-    SearchPath(map: IMap, start: IMapElement, end: IMapElement): IMapElement[];
+    searchPath(map: IMap, start: IMapElement, end: IMapElement): IMapElement[];
 
-    HasPath(map: IMap, start: IMapElement, end: IMapElement): boolean;
+    hasPath(map: IMap, start: IMapElement, end: IMapElement): boolean;
 
-    IsNeighbors(map: IMap, first: IMapElement, second: IMapElement): boolean;
+    areNeighbors(map: IMap, first: IMapElement, second: IMapElement): boolean;
 
-    GetNeighbors(map: IMap, element: IMapElement): IMapElement[];
+    getNeighbors(map: IMap, element: IMapElement): IMapElement[];
 }
