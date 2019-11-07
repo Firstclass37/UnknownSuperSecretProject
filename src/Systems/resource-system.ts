c6f152a04c873480cb8dc503129b225ccb5fc691
Firstclass37 <vankiselev@mail.ru>
2019-11-08 01:02:28 +0300

    add resource system

--- src/Systems/resource-system.ts
+++ src/Systems/resource-system.ts
@@ -1,7 +1,24 @@
 import { ISystem, IEngine } from "adane-ecs"
+import { PlayerComponent } from "../Components/player-component";
+import { PositionComponent } from "../Components/position-component";
+import { ResourceComponent } from "../Components/resource-component";
+import { PlayerStatsComponent } from "../Components/player-stats-component";
 
 export class ResourceSystem implements ISystem{
     update(engine: IEngine): void {
-        throw new Error("Method not implemented.");
+        let player = engine.entities.findOne(PlayerComponent);
+        let playerPosition = player.get<PositionComponent>(PositionComponent.name);
+
+        let resources = engine.entities.findMany(ResourceComponent);
+
+        for(let i = 0; resources.length; i++){
+            let resource = resources[i];
+            if (playerPosition.position != resource.get<PositionComponent>(PositionComponent.name).position){
+                continue;
+            }
+
+            let stats = player.get<PlayerStatsComponent>(PlayerStatsComponent.name);
+            stats.resource += resource.get<ResourceComponent>(ResourceComponent.name).count;
+        }
     }
 }
