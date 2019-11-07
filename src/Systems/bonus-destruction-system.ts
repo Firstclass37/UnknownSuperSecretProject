307aacdf0a3818d6d59c57342a43bd63eb87b83b
Firstclass37 <vankiselev@mail.ru>
2019-11-08 01:26:45 +0300

    add bonus destruction system template

--- src/Systems/bonus-destruction-system.ts
+++ src/Systems/bonus-destruction-system.ts
@@ -1,8 +1,19 @@
 import { ISystem, IEngine } from "adane-ecs"
+import { BonusComponent } from "../Components/bonus-component";
+import { DestructionComponent } from "../Components/destruction-component";
 
 export class BonusDestructionSystem implements ISystem{
-    update(engine: IEngine): void {
-        throw new Error("Method not implemented.");
-    }
 
+    update(engine: IEngine): void {
+        let bonuses = engine.entities.findMany(BonusComponent);
+
+        for(let i = 0; bonuses.length; i++){
+            let bonus = bonuses[i];
+            let destruction = bonus.get<DestructionComponent>(DestructionComponent.name);
+
+            if (destruction.destructed){
+                //some logic for destruction   
+            }
+        }
+    }
 }
