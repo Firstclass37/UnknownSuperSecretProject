137377633254efece6c967b252a6ad075e86bc57
Firstclass37 <vankiselev@mail.ru>
2019-11-10 22:29:02 +0300

    refactoring

--- src/Systems/render-system.ts
+++
@@ -1,7 +0,0 @@
-import { ISystem, IEngine } from "adane-ecs"
-
-export class RenderSystem implements ISystem {
-    update(engine: IEngine): void {
-        throw new Error("Method not implemented.");
-    }
-}
