6b3b63badd758e69509088fe91e6e3de92210385
Firstclass37 <vankiselev@mail.ru>
2019-11-17 22:55:50 +0300

    some logic in bootstrap

--- src/Systems/bootstrap-system.ts
+++ src/Systems/bootstrap-system.ts
@@ -1,8 +1,14 @@
 import { ISystem, IEngine } from "adane-ecs";
+import { InitSystem } from "./init-system";
 
 export class BootstrapSystem implements ISystem{
 
     update(engine: IEngine): void {
+
+
+        if (true){
+            engine.addSystem(new InitSystem());
+        }
     }
 
 }
