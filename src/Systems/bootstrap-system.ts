d55f144f616033e7fab019e113aace801e698124
Firstclass37 <vankiselev@mail.ru>
2019-11-20 23:12:10 +0300

    refactoring

--- src/Systems/bootstrap-system.ts
+++ src/Systems/bootstrap-system.ts
@@ -10,19 +10,19 @@ export class BootstrapSystem implements ISystem, IInitializableEvent{
 
     initialize(engine: IEngine): void {
 
-        // Task.start(this, new LoadAssetTask('squareAssetLoading', ['./assets/square.png']))
-        //     .onComplete((t) => engine.addSystem(new MapBuildSystem()))
+        Task.start(this, new LoadAssetTask('squareAssetLoading', ['./assets/square.png']))
+            .onComplete((t) => engine.addSystem(new MapBuildSystem()))
 
-        Task.start(this, new LoadAssetTask('test', ['./assets/simplePlayer.png']))
-            .onComplete((t) => engine.entities.add(new Entity(new RenderableComponent(Renderable.define((factory) => factory.sprite( { texture: "./assets/simplePlayer.png", name: "mysprite"} ))))));
+        // Task.start(this, new LoadAssetTask('test', ['./assets/simplePlayer.png']))
+        //     .onComplete((t) => engine.entities.add(new Entity(new RenderableComponent(Renderable.define((factory) => factory.sprite( { texture: "./assets/simplePlayer.png", name: "mysprite"} ))))));
     }
 
     update(engine: IEngine): void {
 
-        let entity = engine.entities.findOne(RenderableComponent);
-        if (entity){
-            entity.get(RenderableComponent).renderable.set(SpriteObject, "mysprite", (x) => x.position = {x: 100, y: 100});
-        }
+        // let entity = engine.entities.findOne(RenderableComponent);
+        // if (entity){
+        //     entity.get(RenderableComponent).renderable.set(SpriteObject, "mysprite", (x) => x.position = {x: 100, y: 100});
+        // }
 
     }
 
