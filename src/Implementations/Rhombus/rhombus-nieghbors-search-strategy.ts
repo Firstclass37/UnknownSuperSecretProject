d288b184d08249edcb26073a2e6280c13b200a92
Firstclass37 <vankiselev@mail.ru>
2019-11-28 23:40:32 +0300

    rework in path searcher for rhombus

--- src/Implementations/Rhombus/rhombus-nieghbors-search-strategy.ts
+++ src/Implementations/Rhombus/rhombus-nieghbors-search-strategy.ts
@@ -17,35 +17,34 @@ export class RhombusNieghborsSearchStrategy implements INieighborsSearchStrategy
     getNieghbors(index: number): IMapElement[]{
         let nieghbors: number[] = [];
 
-        for(let i = 0; i < this.map.map.length; i++){
-            let hasLeft = this.even 
-                ? (i + this.map.width - 1) % (2 * this.map.width - 1) == 0 
-                : i % (2 * this.map.width - 1) == 0;
-            let hasRight = this.even 
-                ? i % (2 * this.map.width - 1) == 0 
-                : (i - this.map.width + 1) % (2 * this.map.width - 1) == 0;
+        let hasLeft = this.even 
+            ? (index + this.map.width - 1) % (2 * this.map.width - 1) == 0 
+            : index % (2 * this.map.width - 1) == 0;
+        let hasRight = this.even 
+            ? index % (2 * this.map.width - 1) == 0 
+            : (index - this.map.width + 1) % (2 * this.map.width - 1) == 0;
 
+        if (hasLeft){
+            nieghbors.push(index - this.map.width);
+            nieghbors.push(index + this.map.width - 1);
+        }
+        if (hasRight){
+            nieghbors.push(index + this.map.width - 1);
+            nieghbors.push(index + this.map.width );    
+        }
+        
+        if (this.diagonalAvailable)
+        {
             if (hasLeft){
-                nieghbors.push(i - this.map.width);
-                nieghbors.push(i + this.map.width - 1);
+                nieghbors.push(index + 1);
             }
             if (hasRight){
-                nieghbors.push(i + this.map.width - 1);
-                nieghbors.push(i + this.map.width );    
+                nieghbors.push(index - 1);
             }
-            
-            if (this.diagonalAvailable)
-            {
-                if (hasLeft){
-                    nieghbors.push(i + 1);
-                }
-                if (hasRight){
-                    nieghbors.push(i - 1);
-                }
-                nieghbors.push(i + this.map.width + this.map.width - 1);
-                nieghbors.push(i - this.map.width - this.map.width - 1);
-            }
+            nieghbors.push(index + this.map.width + this.map.width - 1);
+            nieghbors.push(index - this.map.width - this.map.width - 1);
         }
+        
         return nieghbors
             .filter(e => e >= 0 && e <= this.map.map.length)
             .map(e => this.map.getElement(e));
