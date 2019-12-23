53615c02f5fecaeee1fcdd06ae3581f6b5e89a18
Firstclass37 <vankiselev@mail.ru>
2019-12-24 00:44:31 +0300

    test camera scroll

--- src/Systems/bootstrap-system.ts
+++ src/Systems/bootstrap-system.ts
@@ -42,7 +42,7 @@ export class BootstrapSystem implements ISystem, IInitializableEvent{
     goNext(engine: IEngine){
         let assets = engine.entities.findOne(AssetBatchComponent).get(AssetBatchComponent).assets;
         engine.entities.add(new Entity(Guid.newGuid(), new SettingsComponent(this.getGameSetting(assets), this.getMapSetting(assets))));
-        engine.entities.add(new Entity(Guid.newGuid(), new TestComponent() ,new InputComponent(new KeyboardInputTrigger(Key.LEFT, Key.LEFT, Key.LEFT))));
+        engine.entities.add(new Entity(Guid.newGuid(), new TestComponent() ,new InputComponent(new KeyboardInputTrigger(Key.RIGHT, null, null))));
         engine.entities.add(new Entity(Guid.newGuid(), new CameraComponent(240, 300, 0, 100), new CameraMoveComponent(true, 0, 0, 5)));
 
         engine.addSystem(new InputTestSystem());
--- src/Systems/camera-scroll-system.ts
+++ src/Systems/camera-scroll-system.ts
@@ -1,21 +1,26 @@
 import { CameraComponent } from "../Components/camera-component";
 import { ISystem, IEngine} from "adane-ecs";
 import { CameraMoveComponent } from "../Components/camera-move-component";
+import { InputComponent } from "adane-ecs-input";
+import { TestComponent } from "../Components/test-component";
 
 
 export class CameraScrollSystem implements ISystem {
 
     update(engine: IEngine): void {
         let cameraMove = engine.entities.findOne(CameraComponent).get(CameraMoveComponent);
-        //detect pressed keys
-        if (true){
+        let keyboard = engine.entities.findOne(InputComponent, TestComponent).get(InputComponent);
+        
+        if (!keyboard.hit){
             return;
         }
         
-        let step = 2;
+        engine.entities.findOne(CameraMoveComponent).get(CameraMoveComponent).followPlayer = false;
 
-        let offsetX = 0;
-        let offsetY = 0;
+        let step = 10;
+
+        let offsetX = -10;
+        let offsetY = -10;
 
         cameraMove.offsetX = offsetX;
         cameraMove.offsetY = offsetY;
