065343670f728b18b5728b583d15d80c6e0acfe7
Firstclass37 <vankiselev@mail.ru>
2020-01-06 02:28:15 +0300

    fix camera size

--- src/Systems/bootstrap-system.ts
+++ src/Systems/bootstrap-system.ts
@@ -42,12 +42,17 @@ export class BootstrapSystem implements ISystem, IInitializableEvent{
 
     goNext(engine: IEngine){
         let assets = engine.entities.findOne(AssetBatchComponent).get(AssetBatchComponent).assets;
-        engine.entities.add(new Entity(Guid.newGuid(), new SettingsComponent(this.getGameSetting(assets), this.getMapSetting(assets))));
+        let settings = this.getGameSetting(assets);
+
+        let cameraWidthPercent = 1;
+        let cameraHeightPercent = 0.6;
+
+        engine.entities.add(new Entity(Guid.newGuid(), new SettingsComponent(settings, this.getMapSetting(assets))));
         engine.entities.add(new Entity(Guid.newGuid(), new TestComponent(Key.RIGHT) ,new InputComponent(new KeyboardInputTrigger(Key.RIGHT, null, null))));
         engine.entities.add(new Entity(Guid.newGuid(), new TestComponent(Key.LEFT) ,new InputComponent(new KeyboardInputTrigger(Key.LEFT, null, null))));
         engine.entities.add(new Entity(Guid.newGuid(), new TestComponent(Key.DOWN) ,new InputComponent(new KeyboardInputTrigger(Key.DOWN, null, null))));
         engine.entities.add(new Entity(Guid.newGuid(), new TestComponent(Key.UP) ,new InputComponent(new KeyboardInputTrigger(Key.UP, null, null))));
-        engine.entities.add(new Entity(Guid.newGuid(), new CameraComponent(240, 300, 0, 100), new CameraMoveComponent(true, 0, 0, 5)));
+        engine.entities.add(new Entity(Guid.newGuid(), new CameraComponent(settings.size.windowWidth * cameraWidthPercent, settings.size.windowHieght * cameraHeightPercent, 0, settings.size.windowHieght - settings.size.windowHieght * cameraHeightPercent), new CameraMoveComponent(true, 0, 0, 5)));
 
         engine.addSystem(new InputTestSystem());
         engine.addSystem(new MapBuildSystem());
