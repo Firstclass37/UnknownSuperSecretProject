8be3b5e38c73d9b592e0364ab02256ce8ed580c7
Firstclass37 <vankiselev@mail.ru>
2020-01-08 20:36:30 +0300

    add template for enemy vision

---
+++ src/Components/enemy-vision-component.ts
@@ -0,0 +1,5 @@
+import { IComponent } from "adane-ecs";
+
+export class EnemyVisionComponent implements IComponent{
+    public vision: number[];
+}
---
+++ src/Systems/enemy-vision-system.ts
@@ -0,0 +1,8 @@
+import { ISystem, IEngine } from "adane-ecs";
+
+export class EnemyVisionSystem implements ISystem{
+    update(engine: IEngine): void {
+        
+    }
+
+}
