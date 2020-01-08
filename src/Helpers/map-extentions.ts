637f97dfb876179d2926e1e9ec992d83f86a6892
Firstclass37 <vankiselev@mail.ru>
2020-01-08 19:41:37 +0300

    refactoring

--- src/Helpers/map-extentions.ts
+++ src/Helpers/map-extentions.ts
@@ -2,8 +2,28 @@ import { IEngine, Entity } from "adane-ecs";
 import { MapElementComponent } from "../Components/map-element-component";
 import { DestructionComponent } from "../Components/destruction-component";
 import { IndexedMap } from "../Implementations/indexed-map";
+import { SettingsComponent } from "../Components/settings-componen";
+import { HexagonGScoreStrategy } from "../Implementations/Hexagon/hexagon-g-score-strategy";
+import { RhombusNieghborsSearchStrategy } from "../Implementations/Rhombus/rhombus-nieghbors-search-strategy";
+import { RhombusFScoreStrategy } from "../Implementations/Rhombus/rhombus-fscore-strategy";
+import { PathSearcher } from "../aStar/path-searcher";
 
 export class MapExtentions{
+
+    public static buildPath(start: number, end: number, engine: IEngine): number[] {
+        let gameSettings = engine.entities.findOne(SettingsComponent).get(SettingsComponent);
+
+        let indexedMap = MapExtentions.createIndexedMap(engine, gameSettings.gameSettings.map.width);
+        let settings = {
+            gScoreStrategy: new HexagonGScoreStrategy(),
+            fScoreStrategy: new RhombusFScoreStrategy(indexedMap),
+            neighborsSearchStrategy: new RhombusNieghborsSearchStrategy(indexedMap, false, false)
+        };
+        let pathSearcher = new PathSearcher();
+        let path = pathSearcher.getPath(indexedMap.getElement(start), indexedMap.getElement(end), settings);
+        return path.map(e => indexedMap.getIndex(e));
+    }
+
     public static createIndexedMap(engine: IEngine, mapWidth: number): IndexedMap {
         let mapElements = engine.entities.findMany(MapElementComponent);
         let map: any = [];
--- src/Systems/movement-system.ts
+++ src/Systems/movement-system.ts
@@ -3,14 +3,9 @@ import { MapElementComponent } from "../Components/map-element-component";
 import { PlayerMoveComponent } from "../Components/player-move-component";
 import { PlayerComponent } from "../Components/player-component";
 import { MapPositionComponent } from "../Components/map-position-component";
-import { PathSearcher } from "../aStar/path-searcher";
-import { RhombusNieghborsSearchStrategy } from "../Implementations/Rhombus/rhombus-nieghbors-search-strategy";
-import { SettingsComponent } from "../Components/settings-componen";
 import { InputComponent } from "adane-ecs-input";
 import { ChangeSpriteComponent } from "../Components/change-sprite-component";
 import { AssetsConsts } from "../assets-consts";
-import { RhombusFScoreStrategy } from "../Implementations/Rhombus/rhombus-fscore-strategy";
-import { HexagonGScoreStrategy } from "../Implementations/Hexagon/hexagon-g-score-strategy";
 import { MapExtentions } from "../Helpers/map-extentions";
 
 export class MovementSystem implements ISystem {
@@ -32,23 +27,9 @@ export class MovementSystem implements ISystem {
             this.show(engine, path, false);
             this.resetMovement(playerMoveComponent);            
         }
-        playerMoveComponent.path = this.buildPath(currentPlayerPosition, targetPosition, engine);
+        playerMoveComponent.path = MapExtentions.buildPath(currentPlayerPosition, targetPosition, engine);
         playerMoveComponent.new = true;
         this.show(engine, playerMoveComponent.path, true);
-    }
-
-    private buildPath(start: number, end: number, engine: IEngine): number[] {
-        let gameSettings = engine.entities.findOne(SettingsComponent).get(SettingsComponent);
-
-        let indexedMap = MapExtentions.createIndexedMap(engine, gameSettings.gameSettings.map.width);
-        let settings = {
-            gScoreStrategy: new HexagonGScoreStrategy(),
-            fScoreStrategy: new RhombusFScoreStrategy(indexedMap),
-            neighborsSearchStrategy: new RhombusNieghborsSearchStrategy(indexedMap, false, false)
-        };
-        let pathSearcher = new PathSearcher();
-        let path = pathSearcher.getPath(indexedMap.getElement(start), indexedMap.getElement(end), settings);
-        return path.map(e => indexedMap.getIndex(e));
     }
 
     private finishMovement(engine: IEngine): void{
